b6ea79fb0d8aad7471f5a801db9627b988f89b1f
CesarBPV <cesarpareja@upeu.edu.pe>
2018-09-16 22:59:19 -0700

    microservicios 4

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -3,21 +3,23 @@
 import os
 
 
-
 class BaseConfig:
-	"""Base configuration"""
-	TESTING = False
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
+    """Configuración base"""
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_secret_key'
+
 
 class DevelopmentConfig(BaseConfig):
-	"""Development configuration"""
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    """Configuración de desarrollo"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
 
 class TestingConfig(BaseConfig):
-	"""Testing configuration"""
-	TESTING = True
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+    """Configuración de pruebas"""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 class ProductionConfig(BaseConfig):
-	"""Production configuration"""
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    """Configuración de producción"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
