c36c0158a310a4822f5e013cb88fa5226da8649f
CesarBPV <cesarpareja@upeu.edu.pe>
2018-11-21 21:00:00 -0800

    one more commit

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -5,6 +5,7 @@ import os
 
 from flask import Flask  # <-- nuevo
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 
 # instanciado la db
@@ -16,6 +17,9 @@ def create_app(script_info=None):
 
     # instanciando la app
     app = Flask(__name__)
+
+    # enable CORS
+    CORS(app)  # nuevo
 
     # estableciendo configuracion
     app_settings = os.getenv('APP_SETTINGS')
